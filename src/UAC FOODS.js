f46b34810da2c8f3c4703b0697ebbaba66cb481d
KINGZY CHUKS <118390318+kingzykesh@users.noreply.github.com>
2024-01-22 02:56:44 +0100

    ProdutCard

---
+++ src/UAC FOODS.js
@@ -0,0 +1,98 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Fan Choco",
+    alias: "Fan Choco",
+    img1:"./2.jpg",
+    //  "https://www.specfictionshop.com/cdn/shop/products/315455127_2253071438203857_6311282012262232749_n_2000x.jpg?v=1669836598",
+    homeworld: "Earth",
+    abilities: [
+      "FanChoco is a chocolate flavoured frozen milk drink.",
+      "It has the goodness and benefits of cocoa. FanChoco comes in 70ml, and 160ml pouches as well as a 180ml Tetrapak. ",
+      "Superhuman strength",
+    ],
+    team: "Fan-milk limited",
+    description:
+      "Jeho himself, also known as Jehoshaphat, is a brilliant inventor, philanthropist, and industrialist. He uses his genius-level intellect and a highly advanced powered armor suit to fight for justice and protect the world from various threats. Stark is a charismatic playboy turned superhero who combines his technological prowess with unwavering determination to uphold the values of heroism and innovation.",
+  },
+  {
+    name: "FanVanille",
+    alias: "FanVanille",
+    abilities: ["FanVanille is Nigeria’s No 1 Frozen flavoured milk brand", "Its unique milk and vanilla flavour combination make for a refreshing on-the-go snack."],
+    team: "Fan-milk limited",
+     img1:"./3.jpg",
+    // "https://i.pinimg.com/originals/5e/fe/55/5efe55d2a7785a311511736a1a37bcbb.jpg",
+    homeworld: "Earth",
+    description:
+      "Pepper Potts, the formidable Rescue, is an accomplished businesswoman and technological genius. She dons a powered armor suit equipped with an array of advanced technology and weaponry, allowing her to stand alongside the Avengers in the battle against supervillains and global threats. Pepper is a determined and fearless heroine with an unwavering commitment to justice.",
+  },
+  {
+    name: "SuperYogo",
+    alias: "SuperYogo",
+    abilities: ["It comes in plain and lemon flavour and is available in 70ml and 160ml pouches as well as 180ml Tetrapak.", "Enjoy natural goodness with this nutritious and refreshing frozen yoghurt snack."],
+    team: "Fan-milk limited",
+    img1: "./4.jpg",
+    homeworld: "Earth",
+
+    description:
+      "James 'Rhodey' Rhodes, also known as War Machine, is a highly skilled pilot and a military expert. He operates a heavily armed powered armor suit that enhances his strength and combat capabilities. With a strong sense of duty and loyalty to his allies, Rhodey plays a crucial role in the Avengers' efforts to maintain peace and protect the world.",
+  },
+  {
+    name: "FanDango",
+    alias: "FanDango",
+    abilities: ["FanDango is a refreshing fruit drink ", "It is enriched in Vitamin C and comes in 2 formats: Pouch & Bottle."],
+    team: "Fan-milk limited",
+    img1: "./5.jpg",
+    homeworld: "Earth",
+
+    description:
+      "Riri Williams, known as Ironheart, is a prodigious young inventor with a genius-level intellect. She has designed her own advanced powered armor suit, which she uses to fight against threats as a member of the Young Avengers. Riri's intelligence, determination, and innovative spirit make her a rising star in the superhero community.",
+  },
+
+  // Spider-Man
+  {
+    name: "SUPERYOGO",
+    alias: "SUPERYOGO",
+    abilities: ["It comes in plain and lemon flavour and is available in 70ml and 160ml pouches as well as 180ml Tetrapak.", "Enjoy natural goodness with this nutritious and refreshing frozen yoghurt snack."],
+    team: "Fan-milk limited",
+    img1: "./6.jpg",
+    homeworld: "Earth",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Miles Morales, the teenage Spider-Man, has bio-electrokinesis powers, in addition to wall-crawling abilities. As a member of the Champions, Miles embodies the spirit of the Spider-Man legacy, fighting for equality and justice in the Marvel Universe while navigating the challenges of being a young hero.",
+  },
+
+  {
+    name: "GO SLO",
+    alias: "GO SLO",
+    abilities: ["GoSlo is Nigeria’s first locally produced premium Ice-cream brand and Fan Milk West Africa’s first purposeful brand.", "Made with delicious, creamy Ice-cream base and inclusions, GoSlo is crafted to calm and delight for an amazing sensory experience."],
+    team: "Fan-milk limited",
+    img1: "./7.jpg",
+    homeworld: "Earth",
+    description:
+      "Cindy Moon, codenamed Silk, is a skilled hero with the power of web-shooting and remarkable superhuman agility. She is an essential member of the Agents of Atlas, using her arachnid-like abilities to combat threats and safeguard the world.",
+  },
+
+  {
+    name: "FANDANGO BOTTLE",
+    alias: "FANDANGO BOTTLE",
+    abilities: ["FanDango is a refreshing fruit drink.", "It is enriched in Vitamin C and comes in 2 formats: Pouch & Bottle."],
+    team: "Fan-milk limited",
+    img1: "./8.jpg",
+    homeworld: "Earth",
+    description:
+      " ",
+  },
+  {
+    name: "FANICE",
+    alias: "FANICE ",
+    abilities: ["It comes in four exciting variants; Vanilla, Strawberry, Banana and Chocolate and is available in 250ml", "t has a 6-month shelf life."],
+    team: "Fan-milk limited",
+    img1: "./9.jpg",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+];
+
+console.log(characters);
